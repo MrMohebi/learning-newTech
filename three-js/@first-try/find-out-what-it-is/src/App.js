9e5d2835e73f571bda3d2a7f86860cc8e109bf30
MrM <MMMohebi@outlook.com>
2021-01-14 17:19:54 +0330

    create sample project
    it doesnt work in react, there is some problem with it --3d model files cant be loaded due to pathing problem, if put model in src-dir it will chang path in compiling, and if put it in public-dir it will load the first file but fails to load other required files--, i think we should use pure js and upload the whole project on subdomain

--- three-js/@first-try/find-out-what-it-is/src/App.js
+++ three-js/@first-try/find-out-what-it-is/src/App.js
@@ -1,9 +1,44 @@
-import React from "react"
+import React, {Component} from 'react';
+import * as THREE from "three"
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
-function App() {
-  return (
-    <div/>
-  );
+function __init__THREE(containerDiv) {
+  let scene = new THREE.Scene();
+
+  const fov = 35;
+  const aspect = containerDiv.clientWidth / containerDiv.clientHeight;
+  const near = 0.1;
+  const far = 400;
+
+  let camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.set(10, 10, 30);
+  camera.rotation.set(0, 20, 0);
+
+  let renderer = new THREE.WebGLRenderer({antialias: true, alpha: true})
+  renderer.setSize(containerDiv.clientWidth, containerDiv.clientHeight);
+  renderer.setPixelRatio(window.devicePixelRatio)
+
+  containerDiv.appendChild(renderer.domElement)
+
+  const renderModelGLTF = new GLTFLoader();
+  console.log(renderModelGLTF);
+  renderModelGLTF.load("./models3d/house/scene.gltf", function (gltf) {
+    console.log(gltf);
+    scene.add(gltf.scene);
+  })
+
 }
 
-export default App;
+class App extends Component {
+  componentDidMount() {
+    __init__THREE(document.querySelector(".main-container-3d"));
+  }
+
+  render() {
+        return (
+            <div className="main-container-3d"/>
+        );
+    }
+}
+
+export default (App);
