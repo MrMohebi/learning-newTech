f357ff061ca7eab557f1d416bb3d2820e8e4a6a0
MrM <MMMohebi@outlook.com>
2021-01-15 21:11:51 +0330

    fix model to glb and works
    use "gltf-pipeline" library and convert gltf to glb then everything fixed, now there is a model that rotate.

--- three-js/@first-try/find-out-what-it-is/src/App.js
+++ three-js/@first-try/find-out-what-it-is/src/App.js
@@ -2,32 +2,52 @@ import React, {Component} from 'react';
 import * as THREE from "three"
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
+let scene;
+let camera;
+let renderer;
+let house;
+
 function __init__THREE(containerDiv) {
-  let scene = new THREE.Scene();
+  scene = new THREE.Scene();
 
   const fov = 35;
   const aspect = containerDiv.clientWidth / containerDiv.clientHeight;
   const near = 0.1;
   const far = 400;
 
-  let camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.set(10, 10, 30);
-  camera.rotation.set(0, 20, 0);
+  camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.set(0, 0, 20);
 
-  let renderer = new THREE.WebGLRenderer({antialias: true, alpha: true})
+  let ambient = new THREE.AmbientLight(0x404040, 3)
+  scene.add(ambient)
+
+  let light = new THREE.DirectionalLight(0x404040,6)
+  light.position.set(10,10,10)
+  scene.add(light)
+
+  renderer = new THREE.WebGLRenderer({antialias: true, alpha: true})
   renderer.setSize(containerDiv.clientWidth, containerDiv.clientHeight);
   renderer.setPixelRatio(window.devicePixelRatio)
 
   containerDiv.appendChild(renderer.domElement)
 
   const renderModelGLTF = new GLTFLoader();
-  console.log(renderModelGLTF);
-  renderModelGLTF.load("./models3d/house/scene.gltf", function (gltf) {
-    console.log(gltf);
-    scene.add(gltf.scene);
-  })
 
+  console.log(renderModelGLTF);
+  renderModelGLTF.load("./models3d/house/final.glb", function (gltf) {
+    scene.add(gltf.scene);
+    house = gltf.scene.children[0];
+    animation()
+  })
 }
+
+function animation(){
+  requestAnimationFrame(animation);
+  house.rotation.z += 0.01;
+  renderer.render(scene, camera)
+}
+
+
 
 class App extends Component {
   componentDidMount() {
